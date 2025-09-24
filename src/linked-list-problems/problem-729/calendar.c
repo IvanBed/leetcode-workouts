8ba1477058fd266a930ef77a9e80ed8aa5d99170
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-09-24 11:58:40 +0300

    Создание директории задачи

--- src/linked-list-problems/problem-729/calendar.c
+++ src/linked-list-problems/problem-729/calendar.c
@@ -26,15 +26,15 @@ typedef struct {
 static struct Interval* createInterval(int start, int end) 
 {
 	
-	struct Interval* interval = (struct Interval *) malloc(sizeof(struct Interval));
-	if (interval == NULL)
-		return NULL;
+    struct Interval* interval = (struct Interval *) malloc(sizeof(struct Interval));
+    if (interval == NULL)
+        return NULL;
 
-	interval->start_time = start;
-	interval->end_time = end;
+    interval->start_time = start;
+    interval->end_time = end;
     interval->next = NULL;
     interval->prev = NULL;
-	return interval;
+    return interval;
 }
 
 static bool addIntervalNextTo(struct Interval *cur_interval, struct Interval *new_interval) 
@@ -69,11 +69,11 @@ MyCalendar* myCalendarCreate()
 {
     MyCalendar *calendar_inst = (MyCalendar*) malloc(sizeof(MyCalendar));
     
-	if (calendar_inst == NULL)
-		return NULL;
-	calendar_inst->interval_list = createInterval(0, 0);
+    if (calendar_inst == NULL)
+        return NULL;
+    calendar_inst->interval_list = createInterval(0, 0);
     calendar_inst->size = 0;
-	return calendar_inst;
+    return calendar_inst;
 }
 
 bool myCalendarBook(MyCalendar *obj, int start_time, int end_time) 
@@ -82,12 +82,12 @@ bool myCalendarBook(MyCalendar *obj, int start_time, int end_time)
 
 	struct Interval *next_to = findPlace(obj->interval_list, start_time, end_time);
 	if (next_to) {
-		struct Interval *new_interval = createInterval(start_time, end_time);
-		addIntervalNextTo(next_to, new_interval);
+        struct Interval *new_interval = createInterval(start_time, end_time);
+        addIntervalNextTo(next_to, new_interval);
         obj->size++;
-		return true;
-	} else 
-		return false;
+        return true;
+    } else 
+        return false;
 }
 
 void myCalendarFree(MyCalendar* obj) 
