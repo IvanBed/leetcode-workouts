8800f9c72db16af1be894d39aab87e664b3c5001
IvanBed <116827491+IvanBed@users.noreply.github.com>
2026-07-24 17:57:10 +0300

    Решение задач на комбинаторные перестановки

---
+++ src/math-problems/problmes-46-47/solution.cpp
@@ -0,0 +1,39 @@
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+*/
+
+class Solution {
+public:
+    
+    void generate_next_perms(std::set<vector<int>> &all_permutations, std::vector<vector<int>> &storage, vector<int> cur_permutation)
+    {
+        for (size_t i = cur_permutation.size() - 1; i > 0; i--)
+        {
+            std::swap(cur_permutation[i], cur_permutation[i - 1]);
+            
+            if (!all_permutations.contains(cur_permutation))
+            {
+                storage.push_back(cur_permutation);
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        std::set<vector<int>> all_permutations;
+        std::vector<vector<int>> temp_storage;
+        temp_storage.push_back(nums);
+        
+        while (!temp_storage.empty())
+        {
+           vector<int> cur_permutation = temp_storage.back();
+           temp_storage.pop_back();
+           all_permutations.insert(cur_permutation);
+           generate_next_perms(all_permutations, temp_storage, cur_permutation);
+        }
+
+        std::copy(all_permutations.begin(), all_permutations.end(), std::back_inserter(temp_storage));
+        return temp_storage;
+    }
+};
